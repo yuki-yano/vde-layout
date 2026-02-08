6ca8c2352aa19fd5113a6b9e6dfadaba02a5cdaf
yuki_yano <yuki.ycino@gmail.com>
2026-02-08 16:40:54 +0900

    style: format core index exports
    - apply prettier formatting to core re-export declarations

--- src/core/index.ts
+++ src/core/index.ts
@@ -4,12 +4,7 @@ export { createLayoutPlan } from "./planner"
 export { emitPlan } from "./emitter"
 export { compileCorePipeline } from "./pipeline"
 
-export type {
-  CompilePresetInput,
-  CompilePresetFromValueInput,
-  CompilePresetSuccess,
-  CompiledPreset,
-} from "./compile"
+export type { CompilePresetInput, CompilePresetFromValueInput, CompilePresetSuccess, CompiledPreset } from "./compile"
 export type { CreateLayoutPlanSuccess, LayoutPlan, PlanNode } from "./planner"
 export type { PlanEmission } from "./emitter"
 export type { CoreError } from "./errors"
